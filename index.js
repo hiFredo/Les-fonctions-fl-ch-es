bdc0371594e99b5297c90cc04864aa97b3f013c4
Mazino <fredozv01@gmail.com>
2024-11-30 11:50:30 +0100

    second

---
+++ index.js
@@ -0,0 +1,11 @@
+/*let maFunction = function () {
+    console.log('test ');
+}
+maFunction()*/
+
+/*let maFunction = () => {
+    console.log('test01')
+}*/
+
+let maFunction = () => console.log('test1000')
+maFunction()
