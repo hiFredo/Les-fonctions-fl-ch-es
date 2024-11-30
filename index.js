f06cebf1c2a446fc5869e02ef3f3a05e6bc8b46d
Mazino <fredozv01@gmail.com>
2024-11-30 13:07:23 +0100

    Fonction closure

--- index.js
+++ index.js
@@ -5,7 +5,28 @@ maFunction()*/
 
 /*let maFunction = () => {
     console.log('test01')
-}*/
+}
 
 let maFunction = () => console.log('test1000')
-maFunction()
+maFunction() */
+
+function bonjour(prenom) {
+    let resultat = "Bonjour " + prenom; //variable locale 
+    let maClosure = () => console.log(resultat);
+    return maClosure;
+}
+
+//let maFunction = bonjour("Paul");
+//maFunction();
+
+function timer() {
+    let seconde = 0;
+    let maClosure = () => {
+        return ++seconde
+    }
+    return maClosure  //closure sauv var local 
+}
+
+let monTimer = timer()
+
+console.log(monTimer()) 
